f0142857f561d7578ae2a91e8c751a5dbd500894
Jack Gelinas <gelinas.j@husky.neu.edu>
2021-04-30 09:45:15 -0500

    adding readme and refactoring file structure

--- buildHelp.sh
+++
@@ -1,8 +0,0 @@
-yarn build
-cp ../orig-web-kg/lib/jquery.js build/static/js/
-cp ../orig-web-kg/lib/jquery-ui.js build/static/js/
-cp ../orig-web-kg/lib/jquery_csv.js build/static/js/
-cp ../orig-web-kg/lib/jquery-ui.css build/static/css/
-cp ../orig-web-kg/lib/wikibase-sdk.min.js build/static/js/
-cp ../orig-web-kg/lib/wikidata-sdk.min.js build/static/js/
-cp node_modules/react-dialog/css/index.css build/static/css/
